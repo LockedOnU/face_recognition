630aa52b76b5560209cfc5c8ca674477b5021504
LockedOnU <lockedonu@gmail.com>
2018-07-04 14:47:43 +0900

    tensorflow_test1

--- crop_image.py
+++ crop_image.py
@@ -1,11 +1,11 @@
 import cv2, os, pathlib
 
-filePath = '../CASIA-WebFace/CASIA-WebFace'
+filePath = '../original_image'
 faceCascade = cv2.CascadeClassifier('../opencv/sources/data/haarcascades/haarcascade_frontalface_default.xml')
 
 for directoryName in os.listdir(filePath):
     celebName = directoryName
-    cascadePath = '../cascadedImage/' + celebName
+    cascadePath = '../cropped_image/' + celebName
     pathlib.Path(cascadePath).mkdir(parents=True, exist_ok=True)
     for fileName in os.listdir(filePath + '/' + celebName):
         imageName = cv2.imread(filePath + '/' + celebName + '/' + fileName)
@@ -24,6 +24,6 @@ for directoryName in os.listdir(filePath):
             os.chdir(cascadePath)
             # cv2.imshow('Faces found', imageName)
             cv2.imwrite(fileName, cropped)
-            os.chdir('../../test')
+            os.chdir('../../opencv_test')
             # cv2.waitKey(500)
             # cv2.destroyAllWindows()
