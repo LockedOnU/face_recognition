ee68b369ff2e7a1e9ebf034286462983402d3c44
LockedOnU <lockedonu@gmail.com>
2018-07-04 10:14:44 +0900

    openCV test3

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import cv2, os, pathlib
 
-filePath = 'images'
+filePath = '../CASIA-WebFace/CASIA-WebFace'
 faceCascade = cv2.CascadeClassifier('../opencv/sources/data/haarcascades/haarcascade_frontalface_default.xml')
 
 for directoryName in os.listdir(filePath):
     celebName = directoryName
-    cascadePath = 'cascaded/' + celebName
+    cascadePath = '../cascadedImage/' + celebName
     pathlib.Path(cascadePath).mkdir(parents=True, exist_ok=True)
     for fileName in os.listdir(filePath + '/' + celebName):
-        imageName = cv2.imread('images/' + celebName + '/' + fileName)
+        imageName = cv2.imread(filePath + '/' + celebName + '/' + fileName)
         gray = cv2.cvtColor(imageName, cv2.COLOR_BGR2GRAY)
         faces = faceCascade.detectMultiScale(
             gray,
@@ -17,14 +17,13 @@ for directoryName in os.listdir(filePath):
             minSize=(30, 30),
             flags=cv2.CASCADE_SCALE_IMAGE
         )
-        for (x, y, w, h) in faces:
-            cv2.rectangle(imageName, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            # cropped = imageName[y - int(h / 4):y + h + int(h / 4), x - int(w / 4):x + w + int(w / 4)]
-            cropped = imageName[y:y+h, x:x+w]
-
-        os.chdir(cascadePath)
-        cv2.imshow('Faces found', imageName)
-        cv2.imwrite(fileName, cropped)
-        os.chdir('../../')
-        cv2.waitKey(500)
-        cv2.destroyAllWindows()
+        if len(faces) > 0:
+            for (x, y, w, h) in faces:
+                # cv2.rectangle(imageName, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cropped = imageName[y:y+h, x:x+w]
+            os.chdir(cascadePath)
+            # cv2.imshow('Faces found', imageName)
+            cv2.imwrite(fileName, cropped)
+            os.chdir('../../test')
+            # cv2.waitKey(500)
+            # cv2.destroyAllWindows()
